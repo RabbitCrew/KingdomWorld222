746f8c58ded0bbaa36fdc737d18df05953c16612
rabbit <akasagarbha9@naver.com>
2023-03-27 07:51:54 +0900

    무한맵생성

--- KingdomWorld/Assets/Scripts/PerlinNoiseMapScripts/PerlinNoiseMapMaker.cs
+++ KingdomWorld/Assets/Scripts/PerlinNoiseMapScripts/PerlinNoiseMapMaker.cs
@@ -7,47 +7,55 @@ public class PerlinNoiseMapMaker : MonoBehaviour
     [SerializeField] private Texture2D gradientTex;
     [SerializeField] private Sprite[] tile;
 
-    [SerializeField] private int chunkSize = 20;
-    [SerializeField] private float zScreenSize;
-    [SerializeField] private float xScreenSize;
-    
-    public int worldSize = 60;
-    public float noiseFreq = 0.38f;
+    [SerializeField] private int chunkSize = 20;    // 청크 사이즈는 20 * 20
+    [SerializeField] private int gradientSize = 1000;
+    [SerializeField] private int worldSize = 80;
+    [SerializeField] private float noiseFreq = 0.12f;
+
+    struct point
+    {
+        public float x { get; }   // 생성자에서만 수정이 가능함. 읽기전용
+        public float z { get; }   // 생성자에서만 수정이 가능함. 읽기전용
+
+        public point(float x, float z)
+        {
+            this.x = x;
+            this.z = z;
+        }
+	}
+
+
     public float seed;
     public Texture2D noiseTexture;
     public GameObject mother;
-
     public GameObject[,] worldChunks;
 
     private Transform cameraTrans;
-    
-	private void Awake()
+    private List<point> pointList = new List<point>();
+    private List<GameObject> falseChunksList = new List<GameObject>();
+
+    private void Awake()
 	{
         // 캐싱~
         cameraTrans = Camera.main.transform;
-
+        // 시드를 랜덤으로 구함. 시드 값에 따라 다른 노이즈가 생성
         seed = Random.Range(-10000, 10000);
-        zScreenSize = Camera.main.orthographicSize * 2;
-        xScreenSize = zScreenSize * Camera.main.aspect;
-
-        Debug.Log(zScreenSize + " " + xScreenSize);
     }
 
     void Start()
     {
-
-
         float[,] fl = GenerateNoiseTexture();
         // 청크를 생성하는 함수
         CreateChunks();
-        GenerateTerrain(DrawNoiseMap(fl, GenerateMap(worldSize, worldSize)));
+        DrawNoiseMap(fl, GenerateMap(worldSize, worldSize));
+        GenerateTerrain();
         mother.transform.eulerAngles = new Vector3(90,0,0);
     }
 	private void Update()
 	{
         RefreshChunks();
     }
-
+    // 청크를 생성하는 함수
 	public void CreateChunks()
 	{
         int numChunksX = worldSize / chunkSize;
@@ -58,9 +66,8 @@ public class PerlinNoiseMapMaker : MonoBehaviour
             for (int y = 0; y < numChunksY; y++)
             {
                 GameObject newChunk = new GameObject();
-                //newChunk.name = x.ToString() + " " + y.ToString();
-                newChunk.transform.position = new Vector3(x * chunkSize, 0, y * chunkSize);
                 newChunk.transform.parent = mother.transform;
+                newChunk.transform.localPosition = new Vector3(x * chunkSize, y * chunkSize, 0);
                 worldChunks[x,y] = newChunk;
             }
         }
@@ -69,38 +76,66 @@ public class PerlinNoiseMapMaker : MonoBehaviour
     // 오클루전 컬링을 위한 함수
     void RefreshChunks()
     {
+        int cameraX = ((int)(cameraTrans.position.x) / chunkSize);
+        int cameraZ = ((int)(cameraTrans.position.z) / chunkSize);
+
+        int cameraXMax = Mathf.RoundToInt(cameraX + 1.8f);
+        int cameraXMin = Mathf.RoundToInt(cameraX  -1.8f);
+        int cameraZMax = Mathf.RoundToInt(cameraZ + 1.8f);
+        int cameraZMin = Mathf.RoundToInt(cameraZ  -1.8f);
+
+ 
+        // 메인 카메라 범위 밖에 있는 맵은 오브젝트를 꺼준다. 
         for (int x = 0; x < worldChunks.GetLength(0); x++)
         {
             for (int y = 0; y < worldChunks.GetLength(1); y++)
             {
-                if (xScreenSize * 2f<
-                    Vector3.Distance(
-                        new Vector3((x * chunkSize) + chunkSize * 0.5f, 0, (y * chunkSize) + chunkSize * 0.5f),
-                        new Vector3(cameraTrans.position.x, 0, cameraTrans.position.z)) ||
-                    zScreenSize * 2f<
-                    Vector3.Distance(
-                        new Vector3((x * chunkSize) + chunkSize * 0.5f, 0, (y * chunkSize) + chunkSize * 0.5f),
-                        new Vector3(cameraTrans.position.x, 0, cameraTrans.position.z)))
+                // 카메라 너머에 있는 청크들은 비활성화 시킨다.
+                if (((int)worldChunks[x, y].transform.localPosition.x / chunkSize >= cameraXMax ||
+                    (int)worldChunks[x, y].transform.localPosition.x / chunkSize < cameraXMin ||
+                    (int)worldChunks[x, y].transform.localPosition.y / chunkSize >= cameraZMax ||
+                    (int)worldChunks[x, y].transform.localPosition.y / chunkSize < cameraZMin))
                 {
-                    worldChunks[x, y].SetActive(false);
-					//               for (int i = 0; i < worldChunks[x,y].transform.childCount; i++)
-					//{
-					//                   worldChunks[x, y].transform.GetChild(i).GetComponent<SpriteRenderer>().enabled = false;
-					//}
-				}
-				else
-				{
-                    worldChunks[x, y].SetActive(true);
-                    //for (int i = 0; i < worldChunks[x, y].transform.childCount; i++)
-                    //{
-                    //    worldChunks[x, y].transform.GetChild(i).GetComponent<SpriteRenderer>().enabled = true;
-                    //}
+                   
+                    if (falseChunksList.FindIndex(a => a.Equals(worldChunks[x, y])) == -1)
+                    {
+                        worldChunks[x, y].SetActive(false);
+                        falseChunksList.Add(worldChunks[x, y]);
+                        int n = pointList.FindIndex(a => a.x == worldChunks[x, y].transform.localPosition.x && a.z == worldChunks[x, y].transform.localPosition.y);
+                        if (n != -1)
+                        { 
+                            pointList.RemoveAt(n);
+                        }
+                    }
+                }
+                // 청크가 필요한 자리에 이미 활성화하고 있지만 pointList에 좌표값이 저장되어있지 않을 때
+                else if (pointList.FindIndex(a => a.x == worldChunks[x, y].transform.localPosition.x && a.z == worldChunks[x, y].transform.localPosition.y) == -1 &&
+                         pointList.Count + falseChunksList.Count < worldChunks.GetLength(0) * worldChunks.GetLength(1))
+                {
+                    RefreshTexture(worldChunks[x,y], worldChunks[x, y].transform.localPosition.x, worldChunks[x, y].transform.localPosition.y);
+                    pointList.Add(new point(worldChunks[x, y].transform.localPosition.x, worldChunks[x, y].transform.localPosition.y));
                 }
             }
         }
-    }
 
-    public void GenerateTerrain(Color[,] color)
+		for (int x = cameraXMin; x < cameraXMax; x++)
+		{
+			for (int z = cameraZMin; z < cameraZMax; z++)
+			{
+				if (pointList.FindIndex(a => a.x == x * chunkSize && a.z == z * chunkSize) == -1)
+				{
+					falseChunksList[0].SetActive(true);
+					falseChunksList[0].transform.localPosition = new Vector3(x * chunkSize, z * chunkSize, 0);
+					RefreshTexture(falseChunksList[0], x * chunkSize, z * chunkSize);
+					pointList.Add(new point(falseChunksList[0].transform.localPosition.x, falseChunksList[0].transform.localPosition.y));
+                    falseChunksList.RemoveAt(0);
+				}
+			}
+		}
+
+	}
+
+    public void GenerateTerrain()
     {
         for (int x = 0; x < worldSize; x++)
         {
@@ -117,39 +152,7 @@ public class PerlinNoiseMapMaker : MonoBehaviour
                 newTile.transform.parent = worldChunks[(int)chunkCoordX, (int)chunkCoordY].transform;
 
                 newTile.AddComponent<SpriteRenderer>();
-                if (color[x,y].grayscale < 0.125)
-                {
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile[0];
-                }
-                else if (color[x,y].grayscale < 0.25)
-                {
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile[1];
-                }
-                else if (color[x,y].grayscale < 0.375)
-                {
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile[2];
-                }
-                else if (color[x,y].grayscale < 0.5)
-                {
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile[3];
-                }
-                else if (color[x,y].grayscale < 0.625)
-                {
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile[4];
-                }
-                else if (color[x,y].grayscale < 0.75)
-                {
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile[5];
-                }
-                else if (color[x,y].grayscale < 0.875)
-                {
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile[6];
-                }
-                else
-                {
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile[7];
-                }
-                newTile.transform.position = new Vector2(x, y);
+                newTile.transform.localPosition = new Vector3(x % chunkSize, y % chunkSize);
             }
         }
     }
@@ -175,6 +178,66 @@ public class PerlinNoiseMapMaker : MonoBehaviour
 
         return noiseMap;
 	}
+
+    private void RefreshTexture(GameObject chunk, float pointX, float pointY)
+	{
+        float[,] noiseMap = new float[chunkSize, chunkSize];
+        float[,] gradientMap = new float[chunkSize, chunkSize];
+        Color[,] color = new Color[chunkSize, chunkSize];
+        for (int x = 0; x < chunkSize; x++)
+		{
+            for (int y = 0; y < chunkSize; y++)
+			{
+                float v = Mathf.PerlinNoise((x + pointX + seed) * noiseFreq, (y + pointY + seed) * noiseFreq);
+                noiseMap[x, y] = v;
+                int xCoord = Mathf.RoundToInt((x + pointX + gradientSize * 0.5f) * (float)gradientTex.width / gradientSize);
+                int yCoord = Mathf.RoundToInt((y + pointY + gradientSize * 0.5f) * (float)gradientTex.height / gradientSize);
+                gradientMap[x, y] = gradientTex.GetPixel(xCoord, yCoord).grayscale;
+                if (gradientMap[x,y] <= 0.3f) { gradientMap[x, y] = 0f; }
+                else if (gradientMap[x,y] >= 0.75f) { gradientMap[x, y] = 0.75f; }
+                float value = noiseMap[x, y] + gradientMap[x, y];
+                value = Mathf.InverseLerp(0, 2, value);
+                color[x, y] = Color.Lerp(Color.black, Color.white, value);
+
+                if (color[x, y].grayscale < 0.125)
+                {
+                    chunk.transform.GetChild(x * chunkSize + y).GetComponent<SpriteRenderer>().sprite = tile[0];
+                }
+                else if (color[x, y].grayscale < 0.25)
+                {
+                    chunk.transform.GetChild(x * chunkSize + y).GetComponent<SpriteRenderer>().sprite = tile[1];
+                }
+                else if (color[x, y].grayscale < 0.375)
+                {
+                    chunk.transform.GetChild(x * chunkSize + y).GetComponent<SpriteRenderer>().sprite = tile[2];
+                }
+                else if (color[x, y].grayscale < 0.5)
+                {
+                    chunk.transform.GetChild(x * chunkSize + y).GetComponent<SpriteRenderer>().sprite = tile[3];
+                }
+                else if (color[x, y].grayscale < 0.625)
+                {
+                    chunk.transform.GetChild(x * chunkSize + y).GetComponent<SpriteRenderer>().sprite = tile[4];
+                }
+                else if (color[x, y].grayscale < 0.75)
+                {
+                    chunk.transform.GetChild(x * chunkSize + y).GetComponent<SpriteRenderer>().sprite = tile[5];
+                }
+                else if (color[x, y].grayscale < 0.875)
+                {
+                    chunk.transform.GetChild(x * chunkSize + y).GetComponent<SpriteRenderer>().sprite = tile[6];
+                }
+                else
+                {
+                    chunk.transform.GetChild(x * chunkSize + y).GetComponent<SpriteRenderer>().sprite = tile[7];
+                }
+
+            }
+		}
+
+
+	}
+
 
     public float[,] GenerateMap(int width, int height)
     {
