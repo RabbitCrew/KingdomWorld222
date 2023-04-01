54dd70829e7457c18949f8a09588d3137dd07066
paggi0721 <selly2021392001@gmail.com>
2023-04-02 00:06:46 +0900

    거래소 시스템 수정

--- KingdomWorld/Assets/Scripts/BuildingScripts/AnExchange.cs
+++ KingdomWorld/Assets/Scripts/BuildingScripts/AnExchange.cs
@@ -14,8 +14,9 @@ public class AnExchange : MonoBehaviour
     public bool IsOpen;
 
     public GameManager gM;
+    public GameObject SMassage;
 
-    int RandomOpen;
+    public int RandomOpen;
 
     private void Start()
     {
@@ -25,14 +26,22 @@ public class AnExchange : MonoBehaviour
     private void Update()
     {
         OpenState();
+
+        RIsOpen();
+    }
+
+    void RIsOpen()
+    {
+        if (gM.dayNightRatio == 1f || gM.dayNightRatio == 0f)
+        {
+            RandomOpen = Random.Range(0, 2);
+        }
     }
 
     void OpenState()
     {
         if (gM.isDaytime == true)
         {
-            RandomOpen = Random.Range(0, 2);
-
             if (RandomOpen == 0)
             {
                 IsOpen = true;
@@ -50,9 +59,15 @@ public class AnExchange : MonoBehaviour
 
     private void OnMouseDown()
     {
+        Debug.Log(IsOpen);
+
         if (IsOpen == true)
         {
             AnExchangeUI.SetActive(true);
+        }
+        else
+        {
+            SMassage.SendMessage("MessageQ", "오늘은 상인이 없는 날입니다");
         }
     }
 
@@ -68,52 +83,136 @@ public class AnExchange : MonoBehaviour
         switch (CellValue)
         {
             case 0:
-                Resource.Resource_Instance.Wheat -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Wheat >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Wheat -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 1:
-                Resource.Resource_Instance.Food -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Food >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Food -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 2:
-                Resource.Resource_Instance.Wood -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Wood >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Wood -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 3:
-                Resource.Resource_Instance.Meat -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Meat >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Meat -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 4:
-                Resource.Resource_Instance.Leather -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Leather >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Leather -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 5:
-                Resource.Resource_Instance.Itronstone -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Itronstone >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Itronstone -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 6:
-                Resource.Resource_Instance.CastIron -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.CastIron >= ResourceCount)
+                {
+                    Resource.Resource_Instance.CastIron -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 7:
-                Resource.Resource_Instance.Cow -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Cow >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Cow -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 8:
-                Resource.Resource_Instance.Sheep -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Sheep >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Sheep -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 9:
-                Resource.Resource_Instance.Cheese -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Cheese >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Cheese -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 10:
-                Resource.Resource_Instance.Fleece -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Fleece >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Fleece -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             case 11:
-                Resource.Resource_Instance.Cloth -= ResourceCount;
-                Resource.Resource_Instance.Gold += ResourceCount;
+                if (Resource.Resource_Instance.Cloth >= ResourceCount)
+                {
+                    Resource.Resource_Instance.Cloth -= ResourceCount;
+                    Resource.Resource_Instance.Gold += ResourceCount;
+                }
+                else
+                {
+                    SMassage.SendMessage("MessageQ", "자원이 부족합니다.");
+                }
                 break;
             default:
                 break;
