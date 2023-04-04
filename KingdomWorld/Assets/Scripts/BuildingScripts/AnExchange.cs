3d0c650bebda4e6b5b12569f2fb6687af795dd2e
rabbit <akasagarbha9@naver.com>
2023-04-04 15:16:13 +0900

    합치기

--- KingdomWorld/Assets/Scripts/BuildingScripts/AnExchange.cs
+++ KingdomWorld/Assets/Scripts/BuildingScripts/AnExchange.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
 
 public class AnExchange : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class AnExchange : MonoBehaviour
     public TMP_Dropdown CellThings;
 
     public int ResourceCount;
+    private float distance = 50f;
 
     public bool IsOpen;
 
@@ -38,27 +40,30 @@ public class AnExchange : MonoBehaviour
 
     void ClickCheck()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !IsPointerOverUIObject())
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            RaycastHit hit;
-
-            if(Physics.Raycast(ray, out hit))
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(ray, distance);
+            for (int i = 0; i < hits.Length; i++)
             {
-                if(hit.collider.gameObject.name == "An exchange")
+                if (hits[i].transform.GetComponent<BuildingColider>() != null)
                 {
-                    if (IsOpen == true)
+                    if (hits[i].collider.gameObject.tag.Equals("AnExchange") && hits[i].transform.GetComponent<BuildingColider>().isSettingComplete)
                     {
-                        AnExchangeUI.SetActive(true);
+                        if (IsOpen == true)
+                        {
+                            AnExchangeUI.SetActive(true);
 
-                        SMassage.SendMessage("MessageQ", "문이 열렸습니다");
-                    }
-                    else
-                    {
-                        AnExchangeUI.SetActive(false);
+                            SMassage.SendMessage("MessageQ", "문이 열렸습니다");
+                        }
+                        else
+                        {
+                            AnExchangeUI.SetActive(false);
 
-                        SMassage.SendMessage("MessageQ", "오늘은 상인이 없는 날입니다");
+                            SMassage.SendMessage("MessageQ", "오늘은 상인이 없는 날입니다");
+                        }
                     }
                 }
             }
@@ -69,7 +74,7 @@ public class AnExchange : MonoBehaviour
     {
         for (int i = 0; i < AnExchangeBP.transform.childCount; i++)
         {
-            if(AnExchangeBP.transform.GetChild(i).name == "An exchange")
+            if(AnExchangeBP.transform.GetChild(i).tag.Equals("AnExchange"))
             {
                 AnExchangeB = AnExchangeBP.transform.GetChild(i).gameObject;
             }
@@ -249,5 +254,21 @@ public class AnExchange : MonoBehaviour
             default:
                 break;
         }
+    }
+
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        //RaycastResult : BaseRaycastModule에서의 히트 결과.
+        List<RaycastResult> results = new List<RaycastResult>();
+        //EventSystem.current은 최근에 발생한 이벤트 시스템을 반환한다.
+        //첫번째 인자값 : 현재 포인터 데이터.
+        //두번째 인자값 : List of 'hits' to populate.
+        //RaycastAll : 모두 설정된 BaseRaycaster를 사용을 통한 해당 씬으로의 레이 캐스팅.
+        // -> 겹쳐있는 오브젝트들이 있다면 겹쳐있는 수로 results의 카운트가 바뀜
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        //Debug.Log(results.Count);
+        return results.Count > 0;
     }
 }
