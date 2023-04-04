a285da0431f89f523e03bd76e6b95508d6da49ee
ino <dyals713@gmail.com>
2023-04-04 17:07:59 +0900

    sub

--- KingdomWorld/Assets/Scripts/BuildingScripts/BuildingNPCSet.cs
+++ KingdomWorld/Assets/Scripts/BuildingScripts/BuildingNPCSet.cs
@@ -35,36 +35,47 @@ public class BuildingNPCSet : MonoBehaviour
         {
             case 0:
                 NPC.gameObject.tag = "StorageNPC";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 1:
                 NPC.gameObject.tag = "CarpenterNPC";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 2:
                 NPC.gameObject.tag = "CheeseNPC";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 3:
                 NPC.gameObject.tag = "FabricNPC";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 4:
                 NPC.gameObject.tag = "FarmNPC";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 5:
                 NPC.gameObject.tag = "HamNPC";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 6:
                 NPC.gameObject.tag = "NPC";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 7:
                 NPC.gameObject.tag = "Hunter";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 8:
                 NPC.gameObject.tag = "MineWorker";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 9:
                 NPC.gameObject.tag = "WoodCutter";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             case 10:
                 NPC.gameObject.tag = "Smith";
+                NPC.gameObject.GetComponent<NPC>().NPCBUildTrigger = true;
                 break;
             default:
                 break;
