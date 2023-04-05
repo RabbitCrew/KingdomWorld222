2b4c6b17e5b8cc15e18cbc6f1036303f26b09761
inP <dyals713@gmail.com>
2023-04-05 19:42:17 +0900

    사냥꾼, 광부

--- KingdomWorld/Assets/Scripts/NPC/NPC.cs
+++ KingdomWorld/Assets/Scripts/NPC/NPC.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class NPC : NPCScrip
 {
     public GameObject TestBuildingTransform;
-    
+    private bool work = false;//출근 체크 변수
     public string NPCClass;
     public delegate void MyDelegate(Transform start, Transform end);
 
@@ -25,17 +25,24 @@ public class NPC : NPCScrip
         if (gameObject.CompareTag("StorageNPC"))
         {
             CargoClass();
-        }else if (gameObject.CompareTag("WoodCutter"))
+        }
+        else if (gameObject.CompareTag("WoodCutter"))
         {
             WoodCutter();
+        }else if (gameObject.CompareTag("Hunter"))
+        {
+            Hunter();
+        }else if (gameObject.CompareTag("FarmNPC"))
+        {
+            Farmer();
+        }
+        else if (gameObject.CompareTag("MineWorker"))
+        {
+            Miner();
         }
     }
-
-    //창고지기 
-    private bool WorkStart = false;
-    void CargoClass()
+    void dayTimeResetPath()
     {
-
         //건물에 배정되었을때 경로수정
         if (NPCBUildTrigger)
         {
@@ -43,7 +50,7 @@ public class NPC : NPCScrip
             currentPathIndex = 0;
             NPCBUildTrigger = false;
         }
-        if(BuildingNum != null)
+        if (BuildingNum != null)
         {
             //낮과밤이 바뀔때 한번만 경로수정
             if (GameManager.instance.isDaytime && !reSetPathTrigger)
@@ -52,12 +59,24 @@ public class NPC : NPCScrip
                 currentPathIndex = 0;
                 reSetPathTrigger = true;
             }
-            else if (!GameManager.instance.isDaytime && reSetPathTrigger)
+            else if (!GameManager.instance.isDaytime && reSetPathTrigger && !work)
             {
                 ResetPath(this.transform, HouseTr);
                 currentPathIndex = 0;
                 reSetPathTrigger = false;
+                work = false; //퇴근
+            }else if (this.transform.position == BuildingNum.transform.position && GameManager.instance.isDaytime)
+            {
+                work = true;//출근
             }
+        }
+
+    }
+    //창고지기 
+    private bool WorkStart = false;
+    void CargoClass()
+    {
+        dayTimeResetPath();
 
             Transform building = null;
             if (GameManager.instance.isDaytime && !WorkStart)
@@ -79,63 +98,42 @@ public class NPC : NPCScrip
                 /*건물에서 자원꺼내기*/
             }
             Move();
-        }
-        
     }
+        
 
     private bool treeCuting = false;
     void WoodCutter()
     {
-        bool workTrigger = false; //출근했는지 체크
-        
         Transform tree = null;
-        if(BuildingNum != null)
+        dayTimeResetPath();
+        
+        if (work)
         {
-            //건물에 배정되었을때 경로수정
-            if (NPCBUildTrigger)
+            if (!treeCuting)//나무탐색
             {
-                ResetPath(this.transform, BuildingNum.transform);
-                NPCBUildTrigger = false;
-            }
-            //낮과밤이 바뀔때 한번만 경로수정
-            if (GameManager.instance.isDaytime && !reSetPathTrigger && !workTrigger)
-            {
-                ResetPath(this.transform, BuildingNum.transform);
-                reSetPathTrigger = true;
-            }
-            else if (!GameManager.instance.isDaytime && reSetPathTrigger)
-            {
-                ResetPath(this.transform, HouseTr);
-                reSetPathTrigger = false;
-                workTrigger = false; //퇴근
-            }
-            if (this.transform.position == BuildingNum.transform.position && GameManager.instance.isDaytime)
-            {
-                workTrigger = true;//출근
-            }
-            if (workTrigger)
-            {
-                if (!treeCuting)//나무탐색
+                Collider[] colliders = Physics.OverlapSphere(this.transform.position, 1000f);
+                foreach (Collider collider in colliders)
                 {
-                    Collider[] colliders = Physics.OverlapSphere(this.transform.position, 1000f);
-                    foreach (Collider collider in colliders)
+                    if (collider.CompareTag("tree"))
                     {
-                        if (collider.CompareTag("tree"))
-                        {
-                            tree = collider.transform;
-                            ResetPath(this.transform, tree);
-                            treeCuting = true;
-                            break;
-                        }
+                        tree = collider.transform;
+                        ResetPath(this.transform, tree);
+                        currentPathIndex = 0;
+                        treeCuting = true;
+                        break;
                     }
                 }
+            }
+            else
+            {
                 if (this.transform.position == tree.position)
                 {
                     StartCoroutine(CuttingTree(3f, tree));
                 }
             }
-            Move();
+            
         }
+        Move();
     }
     private IEnumerator CuttingTree(float delay, Transform tree)
     {
@@ -144,5 +142,80 @@ public class NPC : NPCScrip
         treeCuting = false;//나무자르기 완료
     }
 
-
+    private bool hunting = false;
+    void Hunter()
+    {
+        dayTimeResetPath();
+        Transform animal = null;
+        if (!hunting)//나무탐색
+        {
+            Collider[] colliders = Physics.OverlapSphere(this.transform.position, 1000f);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.CompareTag("Animal"))
+                {
+                    animal = collider.transform;
+                    ResetPath(this.transform, animal);
+                    currentPathIndex = 0;
+                    hunting = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            if (this.transform.position == animal.position)
+            {
+                StartCoroutine(HuntingAnimal(3f, animal));
+            }
+        }
+        Move();
+    }
+    private IEnumerator HuntingAnimal(float delay, Transform animal)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(animal);
+        hunting = false;//동물 사냥 완료
+    }
+    
+    private bool mining = false;
+    void Farmer()
+    {
+        dayTimeResetPath();
+        Move();
+    }
+    void Miner()
+    {
+        dayTimeResetPath();
+        Transform stone = null;
+        if (!mining)//광물탐색
+        {
+            Collider[] colliders = Physics.OverlapSphere(this.transform.position, 1000f);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.CompareTag("stone"))
+                {
+                    stone = collider.transform;
+                    ResetPath(this.transform, stone);
+                    currentPathIndex = 0;
+                    mining = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            if (this.transform.position == stone.position)
+            {
+                StartCoroutine(MiningStone(3f, stone));
+            }
+        }
+        Move();
+    }
+    private IEnumerator MiningStone(float delay, Transform stone)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(stone);
+        mining = false;//바위 채광 완료
+    }
 }
