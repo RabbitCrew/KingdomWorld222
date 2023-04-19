0cd734924aec600cce44809d91fb4d8a224811ba
rabbit <akasagarbha9@naver.com>
2023-04-19 15:59:21 +0900

    비 구현

--- KingdomWorld/Assets/Scripts/Map/TImeLight2D.cs
+++ KingdomWorld/Assets/Scripts/Map/TImeLight2D.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.ParticleSystemJobs;
 
 //시간대에 따라 Light2D의 색을 바꾸어 낮밤효과를 줌
 public class TImeLight2D : MonoBehaviour
@@ -9,13 +10,15 @@ public class TImeLight2D : MonoBehaviour
     [SerializeField] private RectTransform clockImageRect;
     [SerializeField] private GameManager gameManager;
     [SerializeField] private Light2D light2D;
+    [SerializeField]private ParticleSystem rainParticle;
 
     public float colorLerp { get; private set; }    // 시간 파트 별로 진행도
+    private ParticleSystem.EmissionModule emissionModule;
     private Color colorDawn;    // 새벽 시간
     private Color colorMorning; // 아침 시간
     private Color colorEvening; // 저녁 시간
     private Color colorNight;   // 밤 시간
-
+    private Color rainColorMinus;   // 비 내릴때 얼마나 흐리게 해줄지 
     private float rotate;
     void Start()
     {
@@ -24,9 +27,12 @@ public class TImeLight2D : MonoBehaviour
         colorMorning = new Color(255 / 255f, 249 / 255f, 219 / 255f);   // 아침 조명 색깔
         colorEvening = new Color(221 / 255f, 149 / 255f, 66 / 255f);    // 저녁 조명 색깔
         colorNight = new Color(7 / 255f, 11 / 255f, 22 / 255f); // 밤 조명 색깔
+        rainColorMinus = new Color(0 / 255f, 0 / 255f, 0 / 255f);
         light2D.color = colorDawn;  // 시간은 0부터 시작하므로 시작 조명은 새벽
         rotate = 45f;
-
+        //isRain = false;
+        emissionModule = rainParticle.emission;
+        StartCoroutine(RainTerm());
     }
 
     // Update is called once per frame
@@ -53,7 +59,7 @@ public class TImeLight2D : MonoBehaviour
         else if (gameManager.dayNightRatio >= 0.4f && gameManager.dayNightRatio < 2f / 3f)
         {
             // 위와 마찬가지로 계산. 저녁시간대가 얼마나 지났는지 퍼센트를 의미
-            colorLerp = Mathf.InverseLerp(0.4f, 2f/3f, gameManager.dayNightRatio);
+            colorLerp = Mathf.InverseLerp(0.4f, 2f / 3f, gameManager.dayNightRatio);
             light2D.color = Color.Lerp(colorEvening, colorNight, colorLerp);
         }
         // 밤에서 새벽(저녁 시간대)
@@ -62,8 +68,9 @@ public class TImeLight2D : MonoBehaviour
             // 밤시간대가 얼마나 지났는지 퍼센트를 의미
             colorLerp = Mathf.InverseLerp(2f / 3f, 1f, gameManager.dayNightRatio);
             light2D.color = Color.Lerp(colorNight, colorDawn, colorLerp);
-
         }
+
+        light2D.color = light2D.color - rainColorMinus;
 
         if (Time.timeScale != 0)
         {
@@ -84,6 +91,60 @@ public class TImeLight2D : MonoBehaviour
                 rotate = Mathf.Lerp(-225, -315, colorLerp);
             }
             clockImageRect.localEulerAngles = new Vector3(0, 0, rotate);
+        }
+    }
+
+    IEnumerator RainTerm()
+    {
+        var one = new WaitForSeconds(1f);
+        int ran = 0;
+        int length = 0;
+        Color oneColor = new Color(1 / 255f, 1 / 255f, 1 / 255f);
+        while (true)
+        {
+            ran = Random.Range(1, 181);
+            length = Random.Range(1, 181);
+            Debug.Log(ran + " " + length);
+            for (int i = 0; i < ran; i++)
+            {
+                yield return one;
+            }
+            GameManager.instance.isRain = true;
+
+            rainParticle.Play();
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i < length / 2)
+                {
+                    if (i < 30)
+                    {
+                        rainColorMinus = new Color(i / 255f, i / 255f, i / 255f);
+                    }
+
+                    if (emissionModule.rateOverTime.constant < 300f )
+                    {
+                        emissionModule.rateOverTime = emissionModule.rateOverTime.constant + 10f;
+                    }
+                }
+                else
+                {
+                    if (rainColorMinus != new Color(0/255f, 0/255f, 0/255f))
+                    {
+                        rainColorMinus -= oneColor;
+                    }
+
+                    if (emissionModule.rateOverTime.constant > 10f)
+                    {
+                        emissionModule.rateOverTime = emissionModule.rateOverTime.constant - 10f;
+                    }
+                }
+                yield return one;
+            }
+
+            rainColorMinus = new Color(0 / 255f, 0 / 255f, 0 / 255f);
+            GameManager.instance.isRain = false;
+            rainParticle.Stop();
         }
     }
 }
